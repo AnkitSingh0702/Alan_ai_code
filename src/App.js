fca54d8167994c669767ab4886df04489d866e24
Ankit Singh <ankitsingh0702@gmail.com>
2022-02-20 12:39:44 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import blog from '../src/blog.gif'
+// import blog from '../src/blog.gif';
+import { useLottie } from "lottie-react";
+import Ai from '../src/Ai.json'
 import './App.css';
 import alanBtn from "@alan-ai/alan-sdk-web";
 import React, { useEffect } from 'react';
 function App() {
+  const options = {
+    animationData: Ai,
+    loop: true,
+    autoplay: true
+  };
+  const { View } = useLottie(options);
   useEffect(() => {
     alanBtn({
       key: 'b76de1bad4cb13585196c9d973ceffa22e956eca572e1d8b807a3e2338fdd0dc/stage',
@@ -16,7 +24,8 @@ function App() {
   return (
     <div className="App">
       
-        <img src={blog} className="App-logo" alt="logo" />
+        {/* <img src={blog} className="App-logo" alt="logo" /> */}
+        {View}
         
     </div>
   );
